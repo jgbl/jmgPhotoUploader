c3e1d7673fb70b6186580e9eae27b9126fad60d4
jhmgbl <jhmgbl@gmail.com>
2016-11-04 12:10:22 +0100

    picasa

---
+++ app/src/main/java/org/de/jmg/jmgphotouploader/Picasa.java
@@ -0,0 +1,15 @@
+package org.de.jmg.jmgphotouploader;
+
+/**
+ * Created by hmnatalie on 04.11.16.
+ */
+
+import com.google.gdata.client.*;
+import com.google.gdata.client.photos.*;
+import com.google.gdata.data.*;
+import com.google.gdata.data.media.*;
+import com.google.gdata.data.photos.*;
+
+public class Picasa
+{
+}
